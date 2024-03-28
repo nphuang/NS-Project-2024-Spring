18b0e7850e22f9e2342a14eb5d8be2b6f7fc75a4
PHH <phqq.cs10@nycu.edu.tw>
2024-03-29 01:58:32 +0800

    readme update

--- curl.py
+++ curl.py
@@ -1,10 +1,10 @@
 import subprocess
 
 def execute_curl_command():
-    socks5_proxy = "localhost:1080"  
-    remote_hostname = "https://www.google.com"  
+    socks5_proxy = "localhost:1080"
+    remote_hostname = "https://www.google.com"
     curl_command = f"curl --socks5-hostname {socks5_proxy} {remote_hostname}"
-    
+
     try:
         output = subprocess.check_output(curl_command, shell=True)
         print(output.decode())
